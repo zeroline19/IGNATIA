0fd268ec8530708c466d23d9a3cc07a1ca7a6ba7
zeroline19 <33699046+zeroline19@users.noreply.github.com>
2019-01-25 10:44:40 +0700

    Update mygame.js

--- mygame.js
+++ mygame.js
@@ -1,71 +1,71 @@
-/*
-BUAT fungsi getStatus
-  TULISKAN "Player's current HP: 'playerHealth'"
-  TULISKAN "Monster's current HP: 'monsterHealth'"
-BUAT fungsi getPlayerProfile
-  TULISKAN '----------Player Profile----------'
-  SIMPAN array playerProfile dengan nama, job, playerAtk, dan playerHealth
-  KEMBALIKAN playerProfile dengan DIPISAH secara koma.
-BUAT fungsi getMonsterProfile
-  TULISKAN '----------Monster Profile----------'
-  SIMPAN array monsterProfile dengan nama, "Attack: "'monsterAtk', dan "Health: "'monsterHealth'
-  KEMBALIKAN playerProfile dengan DIPISAH secara koma.
+
+// BUAT fungsi getStatus
+//   TULISKAN "Player's current HP: 'playerHealth'"
+//   TULISKAN "Monster's current HP: 'monsterHealth'"
+// BUAT fungsi getPlayerProfile
+//   TULISKAN '----------Player Profile----------'
+//   SIMPAN array playerProfile dengan nama, job, playerAtk, dan playerHealth
+//   KEMBALIKAN playerProfile dengan DIPISAH secara koma.
+// BUAT fungsi getMonsterProfile
+//   TULISKAN '----------Monster Profile----------'
+//   SIMPAN array monsterProfile dengan nama, "Attack: "'monsterAtk', dan "Health: "'monsterHealth'
+//   KEMBALIKAN playerProfile dengan DIPISAH secara koma.
   
-SIMPAN 'nama' di kolom nama.
-SIMPAN salah satu dari tiga 'job'(Warrior, Priest, atau Wizard) di kolom job. 
-SIMPAN 'playerLevel' di kolom playerLevel berdasarkan 101 DIKALI angka ACAK dengan TRUNCATE.
-SIMPAN 'playerAtk' di kolom playerAtk berdasarkan 100 DITAMBAH playerLevel.
-SIMPAN 'playerHealth' di kolom playerHealth berdasarkan 1000 DIKALI angka ACAK dengan TRUNCATE.
-SIMPAN 'nama monster' di kolom monsterName.
-SIMPAN 'monsterHealth' di kolom monsterHealth berdasarkan 1000 DIKALI angka ACAK dengan TRUNCATE.
-SIMPAN 'monsterAtk' di kolom monsterAtk berdasarkan 100 dipangkat angka ACAK dengan TRUNCATE.
+// SIMPAN 'nama' di kolom nama.
+// SIMPAN salah satu dari tiga 'job'(Warrior, Priest, atau Wizard) di kolom job. 
+// SIMPAN 'playerLevel' di kolom playerLevel berdasarkan 101 DIKALI angka ACAK dengan TRUNCATE.
+// SIMPAN 'playerAtk' di kolom playerAtk berdasarkan 100 DITAMBAH playerLevel.
+// SIMPAN 'playerHealth' di kolom playerHealth berdasarkan 1000 DIKALI angka ACAK dengan TRUNCATE.
+// SIMPAN 'nama monster' di kolom monsterName.
+// SIMPAN 'monsterHealth' di kolom monsterHealth berdasarkan 1000 DIKALI angka ACAK dengan TRUNCATE.
+// SIMPAN 'monsterAtk' di kolom monsterAtk berdasarkan 100 dipangkat angka ACAK dengan TRUNCATE.
 
-IF nama sama dengan kosong THEN
-  TULISKAN 'Please write your name.'
-ELSE IF job sama dengan kosong THEN
-  TULISKAN 'Hello 'nama', please choose your class to start the game.'
-  TULISKAN 'You can choose one of the 3 following classes: Warrior, Priest, and Wizard.'
-END IF
-ELSE IF nama tidak sama dengan kosong dan job sama dengan 'Warrior' THEN
-  TULISKAN 'Welcome to the world of Ignatia, 'nama'!'
-  TULISKAN 'As a 'job', fight your enemies hard with your weapon of choice!
-END IF
-ELSE IF nama tidak sama dengan kosong dan job sama dengan 'Priest' THEN
-  TULISKAN 'Welcome to the world of Ignatia, 'nama'.'
-  TULISKAN 'As a 'job', support your wounded companions! Protect the order!'
-END IF
-ELSE IF nama tidak sama dengan kosong dan job sama dengan 'Wizard' THEN
-  TULISKAN 'Welcome to the world of Ignatia, 'nama'.'
-  TULISKAN As a 'job', you can change the tide of war to your favor!'
-END IF
+// IF nama sama dengan kosong THEN
+//   TULISKAN 'Please write your name.'
+// ELSE IF job sama dengan kosong THEN
+//   TULISKAN 'Hello 'nama', please choose your class to start the game.'
+//   TULISKAN 'You can choose one of the 3 following classes: Warrior, Priest, and Wizard.'
+// END IF
+// ELSE IF nama tidak sama dengan kosong dan job sama dengan 'Warrior' THEN
+//   TULISKAN 'Welcome to the world of Ignatia, 'nama'!'
+//   TULISKAN 'As a 'job', fight your enemies hard with your weapon of choice!
+// END IF
+// ELSE IF nama tidak sama dengan kosong dan job sama dengan 'Priest' THEN
+//   TULISKAN 'Welcome to the world of Ignatia, 'nama'.'
+//   TULISKAN 'As a 'job', support your wounded companions! Protect the order!'
+// END IF
+// ELSE IF nama tidak sama dengan kosong dan job sama dengan 'Wizard' THEN
+//   TULISKAN 'Welcome to the world of Ignatia, 'nama'.'
+//   TULISKAN As a 'job', you can change the tide of war to your favor!'
+// END IF
 
-IF nama tidak sama dengan kosong dan job sama dengan tidak kosong
-  TULISKAN 'Battle Commence!'
-  FOR index sama dengan 0, LEBIH KECIL SAMA DENGAN 500, dan index DITAMBAH 1
-    IF index MODULUS playerAtk sama dengan 0 THEN
-      TULISKAN "'job' 'nama' is attacking 'monsterName'!"
-      KURANGI monsterHealth dengan playerAtk
-      PANGGIL getStatus
-    END IF
-    ELSE IF index MODULUS monsterAtk sama dengan 0 THEN
-      TULISKAN "'monsterName' is attacking 'job' 'nama'!""
-      KURANGI playerHealth dengan monsterAtk
-      PANGGIL getStatus
-    END IF
-    ELSE IF index MODULUS playerAtk MODULUS monsterAtk sama dengan 0 THEN
-      TAMBAHKAN playerHealth dengan monsterAtk
-      TAMBAHKAN monsterHealth dengan playerAtk
-      PANGGIL getStatus
-    END IF
-  END FOR
-  IF monsterHealth LEBIH KECIL dari playerHealth THEN
-    TULISKAN 'Congratulation! 'job' 'nama' You've won the game!'
-  END IF
-  ELSE IF playerHealth LEBIH KECIL dari monsterHealth THEN
-    TULISKAN 'Aww too bad, 'job' 'nama' has been defeated by 'monsterName'...try again next time!'
-  END IF
-END IF
-*/
+// IF nama tidak sama dengan kosong dan job sama dengan tidak kosong
+//   TULISKAN 'Battle Commence!'
+//   FOR index sama dengan 0, LEBIH KECIL SAMA DENGAN 500, dan index DITAMBAH 1
+//     IF index MODULUS playerAtk sama dengan 0 THEN
+//       TULISKAN "'job' 'nama' is attacking 'monsterName'!"
+//       KURANGI monsterHealth dengan playerAtk
+//       PANGGIL getStatus
+//     END IF
+//     ELSE IF index MODULUS monsterAtk sama dengan 0 THEN
+//       TULISKAN "'monsterName' is attacking 'job' 'nama'!""
+//       KURANGI playerHealth dengan monsterAtk
+//       PANGGIL getStatus
+//     END IF
+//     ELSE IF index MODULUS playerAtk MODULUS monsterAtk sama dengan 0 THEN
+//       TAMBAHKAN playerHealth dengan monsterAtk
+//       TAMBAHKAN monsterHealth dengan playerAtk
+//       PANGGIL getStatus
+//     END IF
+//   END FOR
+//   IF monsterHealth LEBIH KECIL dari playerHealth THEN
+//     TULISKAN 'Congratulation! 'job' 'nama' You've won the game!'
+//   END IF
+//   ELSE IF playerHealth LEBIH KECIL dari monsterHealth THEN
+//     TULISKAN 'Aww too bad, 'job' 'nama' has been defeated by 'monsterName'...try again next time!'
+//   END IF
+// END IF
+
 
 function spasi(){
     console.log('');
